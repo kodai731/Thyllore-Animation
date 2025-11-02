717d5f5a570ced6c2fe5d60a703bb4760ee53533
kodai731 <kodai731@gmail.com>
2025-11-02 11:35:02 +0900

    mesh node load

--- src/fbx/fbx.rs
+++ src/fbx/fbx.rs
@@ -1,9 +1,20 @@
+/*
+reference from bevy_mod_fbx
+https://github.com/FizzWizZleDazzle/bevy_mod_fbx/blob/main/src/loader.rs#L217
+ */
 use crate::log;
 use crate::math::math::*;
 use anyhow::{anyhow, Result};
 use cgmath::{Matrix4, Quaternion};
+use fbxcel::tree::v7400::NodeHandle;
 use fbxcel_dom::any::AnyDocument;
-use fbxcel_dom::v7400::object::TypedObjectHandle;
+use fbxcel_dom::v7400::data::{mesh::layer::TypedLayerElementHandle, texture::WrapMode};
+use fbxcel_dom::v7400::object::property::loaders::StrictF64Loader;
+use fbxcel_dom::v7400::object::{
+    model::{ModelHandle, TypedModelHandle},
+    ObjectHandle, TypedObjectHandle,
+};
+use std::ptr::{null, null_mut};
 
 pub unsafe fn load_fbx(path: &str) -> anyhow::Result<()> {
     let file = std::fs::File::open(path)?;
@@ -11,28 +22,11 @@ pub unsafe fn load_fbx(path: &str) -> anyhow::Result<()> {
     match AnyDocument::from_reader(reader).expect("failed to load FBX document") {
         AnyDocument::V7400(fbx_ver, doc) => {
             for object in doc.objects() {
-                let class = object.class();
-                let subclass = object.subclass();
-                if class == "Model" && subclass == "Mesh" {
-                    log!("Mesh found: {:?}", subclass);
-                    if let TypedObjectHandle::Geometry(geometry_handle) = object.get_typed() {
-                        log!("Geometry found: {:?}", geometry_handle);
-                    }
-                    if let TypedObjectHandle::Model(model_handle) = object.get_typed() {
-                        log!("Model found: {:?}", model_handle);
-                        for model_sources in model_handle.source_objects() {
-                            if let Some(source_handle) = model_sources.object_handle() {
-                                if let TypedObjectHandle::Geometry(geometry_handle) =
-                                    source_handle.get_typed()
-                                {
-                                    log!("Geometry found: {:?}", geometry_handle);
-                                }
-                            }
-                        }
-                    }
-                    if let TypedObjectHandle::Material(material_handle) = object.get_typed() {
-                        log!("Material found: {:?}", material_handle);
-                    }
+                if let TypedObjectHandle::Model(TypedModelHandle::Mesh(mesh)) = object.get_typed() {
+                    log!("Loading mesh {:?}", mesh);
+                    let mut fbx_data = FbxData::new(object.node());
+                    fbx_data.name = mesh.name().expect("mesh name not found").to_string();
+                    log!("mesh node name {}", fbx_data.name);
                 }
             }
         }
@@ -42,9 +36,21 @@ pub unsafe fn load_fbx(path: &str) -> anyhow::Result<()> {
 }
 
 #[derive(Clone, Debug, Default)]
-struct FbxData {
-    pub positions: Vec<[f32; 3]>,
-    pub normals: Vec<[f32; 3]>,
-    pub texcoords: Vec<[f32; 2]>,
-    pub indices: Vec<u32>,
+pub struct FbxModel<'a> {
+    fbx_data: Vec<FbxData<'a>>,
+}
+
+#[derive(Clone, Debug)]
+struct FbxData<'a> {
+    pub name: String,
+    pub mesh_node_handle: NodeHandle<'a>,
+}
+
+impl<'a> FbxData<'a> {
+    pub fn new(node_handle: NodeHandle<'a>) -> Self {
+        Self {
+            name: String::new(),
+            mesh_node_handle: node_handle,
+        }
+    }
 }

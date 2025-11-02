6e4da006c4060392c4b9110c6243fc3eb279fb89
kodai731 <kodai731@gmail.com>
2025-11-02 20:01:07 +0900

    retrive indices

--- src/fbx/fbx.rs
+++ src/fbx/fbx.rs
@@ -1,20 +1,22 @@
 /*
-reference from bevy_mod_fbx
+reference from bevy_mod_fbx, FizzWizZleDazzle
 https://github.com/FizzWizZleDazzle/bevy_mod_fbx/blob/main/src/loader.rs#L217
  */
 use crate::log;
 use crate::math::math::*;
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use cgmath::{Matrix4, Quaternion};
 use fbxcel::tree::v7400::NodeHandle;
 use fbxcel_dom::any::AnyDocument;
-use fbxcel_dom::v7400::data::{mesh::layer::TypedLayerElementHandle, texture::WrapMode};
+use fbxcel_dom::v7400::data::{
+    mesh::{layer::TypedLayerElementHandle, PolygonVertexIndex, PolygonVertices},
+    texture::WrapMode,
+};
 use fbxcel_dom::v7400::object::property::loaders::StrictF64Loader;
 use fbxcel_dom::v7400::object::{
     model::{ModelHandle, TypedModelHandle},
     ObjectHandle, TypedObjectHandle,
 };
-use std::ptr::{null, null_mut};
 
 pub unsafe fn load_fbx(path: &str) -> anyhow::Result<()> {
     let file = std::fs::File::open(path)?;
@@ -27,12 +29,190 @@ pub unsafe fn load_fbx(path: &str) -> anyhow::Result<()> {
                     let mut fbx_data = FbxData::new(object.node());
                     fbx_data.name = mesh.name().expect("mesh name not found").to_string();
                     log!("mesh node name {}", fbx_data.name);
+                    let mesh_handle = mesh.geometry().context("failed to get geometry handle")?;
+                    let polygon_vertices = mesh_handle
+                        .polygon_vertices()
+                        .context("failed to get polygon vertices")?;
+                    let triangle_indices = polygon_vertices.triangulate_each(triangulate)?;
+                    log!("polygon vertices {:?}", triangle_indices);
                 }
             }
         }
         _ => log!("unsupported FBX version"),
     }
     Ok(())
+}
+pub fn triangulate(
+    pvs: &PolygonVertices<'_>,
+    poly_pvis: &[PolygonVertexIndex],
+    results: &mut Vec<[PolygonVertexIndex; 3]>,
+) -> anyhow::Result<()> {
+    macro_rules! get_vec {
+        ($pvii:expr) => {
+            get_vec(pvs, poly_pvis[$pvii])
+        };
+    }
+
+    match poly_pvis.len() {
+        3 => {
+            // Got a triangle, no need of triangulation.
+            results.push([poly_pvis[0], poly_pvis[1], poly_pvis[2]]);
+
+            Ok(())
+        }
+        4 => {
+            // p0, p1, p2, p3: vertices of the quadrangle (angle{0..3}).
+            let p0 = get_vec!(0)?;
+            let p1 = get_vec!(1)?;
+            let p2 = get_vec!(2)?;
+            let p3 = get_vec!(3)?;
+
+            // n1: Normal vector calculated with two edges of the angle1.
+            // n3: Normal vector calculated with two edges of the angle3.
+            let n1 = (p0 - p1).cross(p1 - p2);
+            let n3 = (p2 - p3).cross(p3 - p0);
+
+            // If both angle1 and angle3 are concave, vectors n1 and n3 are
+            // oriented in the same direction and `n1.dot(n3)` will be positive.
+            // If either angle1 or angle3 is concave, vector n1 and n3 are
+            // oriented in the opposite directions and `n1.dot(n3)` will be
+            // negative.
+            // It does not matter when the vertices of quadrangle is not on the
+            // same plane, because whichever diagonal you choose, the cut will
+            // be inaccurate.
+            if n1.dot(n3) >= 0.0 {
+                // Both angle1 and angle3 are concave.
+                // This means that either angle0 or angle2 can be convex.
+                // Cut from p0 to p2.
+                results.extend_from_slice(&[
+                    [poly_pvis[0], poly_pvis[1], poly_pvis[2]],
+                    [poly_pvis[2], poly_pvis[3], poly_pvis[0]],
+                ]);
+            } else {
+                // Either angle1 or angle3 is convex.
+                // Cut from p1 to p3.
+                results.extend_from_slice(&[
+                    [poly_pvis[0], poly_pvis[1], poly_pvis[3]],
+                    [poly_pvis[3], poly_pvis[1], poly_pvis[2]],
+                ]);
+            }
+            Ok(())
+        }
+        n => {
+            let points = (0..n).map(|i| get_vec!(i)).collect::<Result<Vec<_>, _>>()?;
+            let points_2d: Vec<_> = {
+                // Reduce dimensions for faster computation.
+                // This helps treat points which are not on a single plane.
+                let (min, max) =
+                    bounding_box(&points).expect("Should never happen: there are 5 or more points");
+
+                let width = max - min;
+
+                match smallest_direction(&width) {
+                    (x) if x.x > 0.0 => points
+                        .into_iter()
+                        .map(|v| Vector2::new(v[1], v[2]))
+                        .collect(),
+                    (x) if x.y > 0.0 => points
+                        .into_iter()
+                        .map(|v| Vector2::new(v[0], v[2]))
+                        .collect(),
+                    (x) => points
+                        .into_iter()
+                        .map(|v| Vector2::new(v[0], v[1]))
+                        .collect(),
+                }
+            };
+            // Normal directions.
+            let normal_directions = {
+                // 0 ... n-1
+                let iter_cur = points_2d.iter();
+
+                // n-1, 0, ... n-2
+                let iter_prev = points_2d.iter().cycle().skip(n - 1);
+
+                // 1, ... n-1, 0
+                let iter_next = points_2d.iter().cycle().skip(1);
+
+                iter_cur
+                    .zip(iter_prev)
+                    .zip(iter_next)
+                    .map(|((cur, prev), next)| {
+                        let prev_cur = *prev - *cur;
+                        let cur_next = *cur - *next;
+                        prev_cur.perp_dot(cur_next) > 0.0
+                    })
+                    .collect::<Vec<_>>()
+            };
+            assert_eq!(normal_directions.len(), n);
+
+            let dirs_true_count = normal_directions.iter().filter(|&&v| v).count();
+
+            if dirs_true_count <= 1 || dirs_true_count >= n - 1 {
+                // Zero or one angles are concave.
+                let minor_sign = dirs_true_count <= 1;
+
+                // If there are no concave angles, use 0 as center.
+                let convex_index = normal_directions
+                    .iter()
+                    .position(|&sign| sign == minor_sign)
+                    .unwrap_or(0);
+
+                let convex_pvi = poly_pvis[convex_index];
+
+                let iter1 = (0..n)
+                    .cycle()
+                    .skip(convex_index + 1)
+                    .take(n - 2)
+                    .map(|i| poly_pvis[i]);
+
+                let iter2 = (0..n).cycle().skip(convex_index + 2).map(|i| poly_pvis[i]);
+
+                for (pvi1, pvi2) in iter1.zip(iter2) {
+                    results.push([convex_pvi, pvi1, pvi2]);
+                }
+
+                Ok(())
+            } else {
+                log!(
+                    "Unsupported polygon: {}-gon with two or more concave angles",
+                    n
+                );
+                Err(anyhow!("Unsupported polygon"))
+            }
+        }
+    }
+}
+
+fn get_vec(pvs: &PolygonVertices<'_>, pvi: PolygonVertexIndex) -> anyhow::Result<Vector3<f32>> {
+    pvs.control_point(pvi)
+        .map(|p| vec3_from_array([p.x as f32, p.y as f32, p.z as f32]))
+        .ok_or_else(|| anyhow!("Index out of range: {pvi:?}"))
+}
+
+fn bounding_box<'a>(
+    points: impl IntoIterator<Item = &'a Vector3<f32>>,
+) -> Option<(Vector3<f32>, Vector3<f32>)> {
+    points.into_iter().fold(None, |minmax, point| {
+        minmax.map_or_else(
+            || Some((*point, *point)),
+            |(min, max)| {
+                Some((
+                    Vector3::new(min.x.min(point.x), min.y.min(point.y), min.z.min(point.z)),
+                    Vector3::new(max.x.max(point.x), max.y.max(point.y), max.z.max(point.z)),
+                ))
+            },
+        )
+    })
+}
+
+fn smallest_direction(v: &Vector3<f32>) -> Vector3<f32> {
+    match () {
+        () if v.x < v.y && v.z < v.x => Vector3::new(0.0, 0.0, 1.0),
+        () if v.x < v.y => Vector3::new(1.0, 0.0, 0.0),
+        () if v.z < v.y => Vector3::new(0.0, 0.0, 1.0),
+        () => Vector3::new(0.0, 1.0, 0.0),
+    }
 }
 
 #[derive(Clone, Debug, Default)]

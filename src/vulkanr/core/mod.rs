f53b2429faddb8b31edb3eb2afd311a57b2bdd76
kodai731 <kodai731@gmail.com>
2025-12-27 21:01:34 +0900

    delete vulkanr core instance

--- src/vulkanr/core/mod.rs
+++ src/vulkanr/core/mod.rs
@@ -1,7 +1,5 @@
 pub mod device;
-pub mod instance;
 pub mod swapchain;
 
 pub use device::*;
-pub use instance::*;
 pub use swapchain::*;
--- src/vulkanr/core/swapchain.rs
+++ src/vulkanr/core/swapchain.rs
@@ -1,9 +1,10 @@
 use super::device::*;
-use super::instance::*;
 use crate::vulkanr::resource::*;
 use crate::vulkanr::render::*;
 use crate::vulkanr::vulkan::*;
 use crate::vulkanr::command::RRCommandPool;
+use winit::window::Window;
+use vulkanalia::Instance;
 
 #[derive(Clone, Debug)]
 pub struct SwapchainSupport {
